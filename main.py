e59d659b339f8f5e983d9962516192777720f45b
Radiance Babajide <tcitrogg@gmail.com>
2025-12-19 19:53:52 +0100

    done bit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # import our modules
 import streamlit as st
+# import pwrd
+from pwrd import Pwrd
 
-vault = {}
+pd = Pwrd()
 
 # gui
 create_tab, fetch_tab = st.tabs(["Create", "Fetch"])
@@ -11,10 +13,17 @@ with create_tab:
     usrname = st.text_input("Username")
     generate_btn = st.button("Generate")
     if generate_btn:
-       passwrd = gen_passwrd()
+       passwrd = pd.gen_passwrd()
+       pd.store(account_name, usrname, passwrd)
        st.code(passwrd)
        
 
 with fetch_tab:
     account_name = st.text_input("Account Name", key="fetch_tab").lower()
     fetch_btn = st.button("Fetch")
+    if fetch_btn and len(account_name) != 0:
+        result = pd.fetch_account(account_name)
+        if result == False:
+            st.error("Invalid Account")
+        else:
+            st.code(result)
--- pwrd.py
+++ pwrd.py
@@ -1,10 +1,15 @@
 import random
 import string
 import json
+import os
+
+SECRET_FILE = ".VAULT.pwrd"
 
 class Pwrd:
     def __init__(self):
-        pass
+        # confirm if our dbfile exists
+        if not os.path.exists(SECRET_FILE):
+            json.dump({}, open(SECRET_FILE, "w"))
 
     def gen_passwrd(self, l_len=6, n_len=1, s_len=1):
         """
@@ -23,3 +28,15 @@ class Pwrd:
         passwrd = "".join(random_chars)
 
         return passwrd
+    
+    def fetch_account(self, account_name):
+        content = json.load(open(SECRET_FILE))
+        if account_name in content:
+            return content[account_name]
+        else:
+            return False
+
+    def store(self, account_name, username, password):
+        content = json.load(open(SECRET_FILE))
+        content[account_name] = {"username":username, "password":password}
+        json.dump(content, open(SECRET_FILE, "w"))
